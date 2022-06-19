5d6d10908ebd9b9172b417526d119cd2a1f8af37
shpicard-dev <s.picard@codeur.online>
2022-06-19 22:40:04 +0200

    insert user in database

--- db_contact-form.sql
+++ db_contact-form.sql
@@ -5,17 +5,25 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-SET NAMES utf8;
-
 DROP TABLE IF EXISTS `tbl_contacts`;
 CREATE TABLE `tbl_contacts` (
   `contact_id` int(11) NOT NULL AUTO_INCREMENT,
   `contact_username` varchar(255) NOT NULL,
   `contact_email` varchar(255) NOT NULL,
-  `contact_subject` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `contact_subject` varchar(255) NOT NULL,
   `contact_message` text NOT NULL,
   PRIMARY KEY (`contact_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE `tbl_users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_username` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 2022-06-13 20:28:12
+
+-- 2022-06-19 20:13:20
--- handler_user-registration.php
+++ handler_user-registration.php
@@ -7,7 +7,28 @@ session_start();
     && isset($_POST['data-password']) && !empty($_POST['data-password']) 
     && isset($_POST['data-pswdConfirmation']) && !empty($_POST['data-pswdConfirmation'])){
 
-        echo 'Ça marche ! 🥳';
+        if ($_POST['data-password'] == $_POST['data-pswdConfirmation']) {
+
+            $user_username = strip_tags($_POST['data-username']);
+            $user_email = strip_tags($_POST['data-email']);
+            $user_unencrypted_password = strip_tags($_POST['data-password']);
+            $user_encrypted_password = password_hash($user_unencrypted_password, PASSWORD_DEFAULT);
+
+            require_once('db_connect.php');
+            $sql = 'INSERT INTO tbl_users(`user_username`, `user_password`, `user_email`) VALUES(:user_username, :user_password, :user_email)';
+            $query = $db->prepare($sql);
+            $query->bindValue(':user_username', $user_username, PDO::PARAM_STR);
+            $query->bindValue(':user_password', $user_encrypted_password, PDO::PARAM_STR);
+            $query->bindValue(':user_email', $user_email, PDO::PARAM_STR);
+            $query->execute();
+
+            $_SESSION['message'] = 'Nouvel utilisateur enregistré !';
+            header('Location: index.php'); 
+
+        } else {
+            $_SESSION['message'] = 'Les mots de passe ne correspondent pas...';
+            header('Location: view_user-registration.php'); 
+        }
 
     } else {
         $_SESSION['message'] = 'Completez tous les champs !';
